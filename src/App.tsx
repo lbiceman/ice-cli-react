86d7c1dab2c57e9f788848579999ffb2d8b20972
liweibing <lbiceman@126.com>
2023-10-11 15:16:06 +0800

    feat: 移除多余组件

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,16 @@ import React, { lazy, Suspense, useState } from 'react'
 import Class from './components/Class'
 import './app.less'
 
-// prefetch
-const PreFetchDemo = lazy(() => import(
-  '@/components/PreFetchDemo'
-))
-// preload
-const PreloadDemo = lazy(() => import(
-  '@/components/PreloadDemo'
- ))
-
 function App() {
   const [ show, setShow ] = useState(false)
 
-  const onClickSSSSSSS = () => {
+  const showComponent = () => {
     setShow(true)
   }
   return (
     <>
-      <h2 onClick={onClickSSSSSSS}>展示</h2>
+      <h2 onClick={showComponent}>展示</h2>
       <Class />
-      {/* show为true时加载组件 */}
-      { show && (
-        <>
-          <div className='smallImg'></div> {/* 小图片背景容器 */}
-          <div className='bigImg'></div> {/* 大图片背景容器 */}
-          <Suspense fallback={null}><PreloadDemo /></Suspense>
-          <Suspense fallback={null}><PreFetchDemo /></Suspense>
-        </>
-      ) }
     </>
   )
 }
